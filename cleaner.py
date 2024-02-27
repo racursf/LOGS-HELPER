878ae684ddff0ed67bf75895af0cc429eb214ff3
racurs <154503026+racursf@users.noreply.github.com>
2024-02-27 18:51:55 +0400

    1.2.0
    Fix cleaner. Add progress bar. Add more extensions. Can filter in extensions.py

--- cleaner.py
+++ cleaner.py
@@ -1,15 +1,22 @@
 import os
 from os import system
-import shutil
+from tqdm import tqdm, tqdm_gui
+from extensions import extension
 def clean():
     system("title " + 'LOGS CLEANER')
     folder_path = input('Введите папку с логами для чистки: ')
-    ext = ['.vdf', '.bak', '.bat', '.png', '.jpeg', '.jpg', '.ico', '.gif', '.svg', '.mp3', '.mp4', '.py', '.avi', '.pdf', '.wav', '.mkv', '.mpg', '.mov', '.3gp', '.js', '.exe', '.rar', '.zip', '.7z', '.cab', '.dll', '.sys', '.msc', '.lnk', '.scr', '.bin', '.url', '.cmd', '.vbs', '.jar', '.jse', '.chm', '.iso', '.torrent', '.tmp', '.php', '.apk', '.ahk', '.scr', '.cab', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pot', '.potx', '.pps', 'ppsx', '.rtf', '.odt']
+    ext = extension
 
     def delete_files_with_extensions(folder_path, extensions):
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                if any(file_path.endswith(ext) for ext in extensions):
-                    os.remove(file_path)
+        deleted_files = 0
+        with tqdm(total=len(extensions), desc="Удаление файлов", unit=' files') as pbar:
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    if any(file_path.endswith(ext) for ext in extensions):
+                        os.remove(file_path)
+                        deleted_files += 1
+                        pbar.update(1)
+        print(f"Удалено {deleted_files} файлов.")
+
     delete_files_with_extensions(folder_path, ext)
